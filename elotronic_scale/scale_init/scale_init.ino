5eb9482cd0cb20e43e8316b7bc914916f56a6d82
nicklee <nicklee7963@gmail.com>
2024-07-30 15:27:40 +0800

    scale done

---
+++ elotronic_scale/scale_init/scale_init.ino
@@ -0,0 +1,28 @@
+#include "HX711.h"
+
+const int DT_PIN = 7;
+const int SCK_PIN = 6;
+const int sample_weight = 93.1;
+
+HX711 scale;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  scale.begin(DT_PIN, SCK_PIN);
+  scale.set_scale();
+  scale.tare();
+  Serial.println("Nothing on it");
+  Serial.println(scale.get_units(10));
+  Serial.println("please put sample");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float current_weight = scale.get_units(10);
+  float scale_factor = (current_weight/sample_weight);
+  Serial.println("Scale number:");
+  Serial.println(scale_factor,0);
+
+}
