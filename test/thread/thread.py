31efb42e79aa5f0b12833f39a9f72381f22eea84
nicklee <nicklee7963@gmail.com>
2024-08-25 20:18:55 +0800

    thread test

--- test/thread/thread.py
+++ test/thread/thread.py
@@ -1,20 +1,31 @@
 import threading
 import time
 
+
 done = False
 
 
+
 def worker(text):
+
     counter = 0
+
     while True:
+
         time.sleep(1)
+
         counter += 1
+
         print(f"{text} : {counter}")
+
 
 
 threading.Thread(target=worker, daemon = True, args = ("ABC",)).start()
 
 
 
+
+
 input("press enter to quit")
+
 done = True
